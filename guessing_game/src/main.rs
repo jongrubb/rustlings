4279bdd83ff7f534e3943489b38c4ebd9463bb8c
Jon Grubb <grubb2k7@gmail.com>
2025-09-20 17:15:20 -0400

    implemented guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,37 @@
+use rand::{Rng, rng};
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rng().random_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{} is not a valid number.", guess.trim());
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess is too small!"),
+            Ordering::Greater => println!("Your guess is too big!"),
+            Ordering::Equal => {
+                println!("Your guess is just right!");
+                break;
+            }
+        }
+    }
 }
