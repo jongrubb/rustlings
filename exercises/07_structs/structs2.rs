220d418b638524ddf70aebb9cb64915929b151a1
Jon Grubb <grubb2k7@gmail.com>
2025-09-21 16:10:54 -0400

    fixed structs exercises

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -36,6 +36,16 @@ mod tests {
         // TODO: Create your own order using the update syntax and template above!
         // let your_order =
 
+        // let mut your_order = create_order_template();
+        // your_order.name = String::from("Hacker in Rust");
+        // your_order.count = 1;
+
+        let your_order = Order {
+            name: String::from("Hacker in Rust"),
+            count: 1,
+            ..create_order_template()
+        };
+
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
