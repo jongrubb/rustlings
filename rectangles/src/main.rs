d01fa541c35f8857098cbf1514afd519a38801fd
Jon Grubb <grubb2k7@gmail.com>
2025-09-21 15:22:04 -0400

    implemented rectangle project

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rectangle = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels. {rectangle:#?}",
+        area(&rectangle)
+    );
+
+    dbg!(rectangle);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
